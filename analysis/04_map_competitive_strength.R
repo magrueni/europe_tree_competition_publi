0dd46755a6df942053241f468fd646fd69292dc2
MarcGruenig <78090519+magrueni@users.noreply.github.com>
2026-03-29 21:16:37 +0200

    revision
    Refactor competitive strength mapping process and update plot saving paths.

--- analysis/04_map_competitive_strength.R
+++ analysis/04_map_competitive_strength.R
@@ -53,18 +53,17 @@ rcps <- c("rcp_8_5", "rcp_4_5", "rcp_2_6")
 ### loop over rcps to do the maps
 for(c in rcps){
   
-  
-  # define output lists
   plot_list <- list()
   hei_plot_list <- list()
   lai_plot_list <- list()
-  
+  rast_1 <- rast()
+  rast_2 <- rast()
   rast_all <- rast()
   min_max <- list()
   
-  
   # loop over the species
   for(s in 1:length(species)){
+    
     
     # naming things
     nam <- gsub("_", " ", species[s])
@@ -76,8 +75,26 @@ for(c in rcps){
     
     # load species range
     sp <- species[s]
-    shp <- terra::vect(paste0(path, "/chorological_maps_dataset/", nam, "/shapefiles/", nam2, "_plg.shp"))
+    # shp <- terra::vect(paste0(path, "/Projects/Resonate/tree_sdms/euforgen/chorological_maps_dataset/", nam, "/shapefiles/", nam2, "_plg.shp"))
+    shp <- vect(paste0(path, "/gis/sp_range_nat/", species[s], "_shape_nat.shp"))
     shp <- terra::project(shp, proj_leae)
+    
+    dom_dist <- read_csv(paste0(path, "/gis_data/range_edges/sp_dom_vals_", species[s], "_v2.csv"))
+    dom_dist <- area_df %>%
+      left_join(dom_dist %>% dplyr::select(point_id, dom), by = c("point_id")) %>%
+      mutate(dom = ifelse(is.na(dom), 0, dom),
+             dom = ifelse(dom < 5, 0, dom),
+             forest_cover = ifelse(forest_cover < 0.05, 0, forest_cover))
+    
+    dom_dist <- dom_dist %>% 
+      mutate(forest_area = forest_cover * area,
+             weighted_dom = (dom/100 * area) / forest_area) %>% 
+      mutate(weighted_dom = ifelse(is.na(weighted_dom), 0, weighted_dom),
+             weighted_dom = ifelse(is.infinite(weighted_dom), 0, weighted_dom),
+             weighted_dom = ifelse(weighted_dom > 1, 1, weighted_dom))
+    
+    dom_dist <- dom_dist %>% dplyr::select(point_id, dom = weighted_dom)
+    min_val <- 0.05
     
     
     # get LAI data for species
@@ -86,25 +103,41 @@ for(c in rcps){
       filter(rcp == c)
     
     # create raster from it
-    r_1 <- left_join(rast_df, na.omit(r_1), by = "point_id") 
-    r_1 <- rast(rasterFromXYZ(r_1[, c("x", "y", "net_change")]))
+    r_1 <- left_join(rast_df, dom_dist, by = c("point_id")) %>% 
+      left_join(., na.omit(r_1), by = "point_id")
+    r_1 <- r_1 %>% 
+      # mutate(weighted_change = net_change * dom/100) 
+      mutate(weighted_change = ifelse(dom < min_val, 0, net_change))
+    
+    r_1 <- rast(rasterFromXYZ(r_1[, c("x", "y", "weighted_change")]))
     crs(r_1) <- proj_wgs
     r_1 <- terra::project(r_1, proj_leae)
-
+    plot(r_1)
+    rast_1 <- c(rast_1, r_1)
+    
     # get height data for the species
     r_2 <- hei %>% 
       filter(species == sp) %>% 
       filter(rcp == c)
     
     # create raster from it
-    r_2 <- left_join(rast_df, na.omit(r_2), by = "point_id") 
-    r_2 <- rast(rasterFromXYZ(r_2[, c("x", "y", "net_change")]))
+    r_2 <- left_join(rast_df, dom_dist, by = c("point_id")) %>% 
+      left_join(., na.omit(r_2), by = "point_id") %>% 
+      mutate(weighted_change = ifelse(dom < min_val, 0, net_change))
+    # r_2 <- r_2 %>% mutate(weighted_change = net_change * dom/100)
+    
+    r_2 <- rast(rasterFromXYZ(r_2[, c("x", "y", "weighted_change")]))
     crs(r_2) <- proj_wgs
     r_2 <- terra::project(r_2, proj_leae)
-
+    plot(r_2)
+    rast_2 <- c(rast_2, r_2)
+    
     # put the rasters together to get the CSI raster
     r_fin <- (r_1 + r_2)/2
-    r_fin <- crop(r_fin, europe)
+    r_fin <- terra::crop(r_fin, europe)
+    r_fin <- terra::mask(r_fin, shp)
+    r_fin <- terra::mask(r_fin, europe)
+    r_fin <- terra::project(r_fin, x_rast_proj)
     r_fin_vect <- r_fin
     r_fin_vect[!is.na(r_fin_vect)] <- 1
     r_fin_vect <- as.polygons(r_fin_vect, dissolve = T)
@@ -114,15 +147,18 @@ for(c in rcps){
     rast_all <- c(rast_all, r_fin)
     
     
-    ###  plotting ---
+    ## plotting ---
     cols <- met.brewer("OKeeffe1", 100, "continuous")
     map_diff <- ggplot(eu_shp_sf_leae) +
       geom_sf(fill = "grey", alpha = 0.7) +
       geom_spatraster(data = r_fin) +
-      scale_fill_gradientn(colours = cols, na.value = NA, limits = c(-80, 80),
-                           breaks = c(-80, 0, 80), 
+      scale_fill_gradientn(colours = cols, na.value = NA, limits = c(-50, 50),
+                           breaks = c(-50, 0, 50), 
                            guide = guide_colorbar(title.position = "top")) +
+      
+      # geom_spatvector(data = shp, fill = "red", alpha = 0.5) +
       geom_spatvector(data = europe, fill = "transparent", col = "black") +
+      # geom_spatvector(data = r_fin_vect, fill = "transparent", col = "black", linewidth = 0.2)  +
       theme_void() +
       labs(
         fill = paste0("Change in competitive strength [%]"),
@@ -133,12 +169,20 @@ for(c in rcps){
         plot.title = element_text(hjust = 0, vjust = -10)
       )
     
+    map_diff  
+    
+    r_1 <- terra::crop(r_1, europe)
+    r_1 <- terra::mask(r_1, shp)
+    r_1 <- terra::mask(r_1, europe)
+    
     map_hei <- ggplot(eu_shp_sf_leae) +
       geom_sf(fill = "grey", alpha = 0.7) +
       geom_spatraster(data = r_1) +
       scale_fill_gradientn(colours = cols, na.value = NA, limits = c(-100, 100),
-                           breaks = c(-100, 0, 100), 
+                           breaks = c(-50, 0, 50), 
                            guide = guide_colorbar(title.position = "top")) +
+      
+      # geom_spatvector(data = shp, fill = "red", alpha = 0.5) +
       geom_spatvector(data = europe, fill = "transparent", col = "black") +
       theme_void() +
       labs(
@@ -150,11 +194,15 @@ for(c in rcps){
         plot.title = element_text(hjust = 0, vjust = -10)
       )
     
+    r_2 <- terra::crop(r_2, europe)
+    r_2 <- terra::mask(r_2, shp)
+    r_2 <- terra::mask(r_2, europe)
+    
     map_lai <- ggplot(eu_shp_sf_leae) +
       geom_sf(fill = "grey", alpha = 0.7) +
       geom_spatraster(data = r_2) +
       scale_fill_gradientn(colours = cols, na.value = NA, limits = c(-100, 100),
-                           breaks = c(-100, 0, 100), 
+                           breaks = c(-50, 0, 50), 
                            guide = guide_colorbar(title.position = "top")) +
       # geom_spatvector(data = shp, fill = "red", alpha = 0.5) +
       geom_spatvector(data = europe, fill = "transparent", col = "black") +
@@ -178,28 +226,29 @@ for(c in rcps){
   
   
   # make the assembled plots
-  legend <- cowplot::get_legend(plot_list[[1]] + theme(legend.position = "bottom",
-                                                       legend.title = element_text(hjust = 2)))
+  legend <- cowplot::get_legend(map_diff + theme(legend.position = "bottom",
+                                                 legend.title = element_text(hjust = 2)))
+  
   p_grid <- cowplot::plot_grid(plotlist = plot_list, ncol = 3)
   csi_plot <- cowplot::plot_grid(p_grid, ncol = 1, rel_heights = c(1, 0.1))
-  ggsave(paste0(path, "/results/maps/prediction_csi_", c, "_all_v46_final.png"),
+  ggsave(paste0(path, "/svd_dnn/svd_dnn_results/maps/prediction_csi_", c, "_all_v46_revision.pdf"),
          csi_plot, dpi = 300, width = 12, height = 12, units = "in")
   
-  ggsave(paste0(path, "/results/maps/prediction_csi_", c, "_all_v46_final_legend.png"),
+  ggsave(paste0(path, "/svd_dnn/svd_dnn_results/maps/prediction_csi_", c, "_all_v46_revision_legend.pdf"),
          legend, dpi = 300, width = 12, height = 12, units = "in")
   
   legend <- cowplot::get_legend(hei_plot_list[[1]] + theme(legend.position = "bottom",
                                                            legend.title = element_text(hjust = 1)))
   p_grid <- cowplot::plot_grid(plotlist = hei_plot_list, ncol = 3)
   hei_plot <- cowplot::plot_grid(p_grid, legend, ncol = 1, rel_heights = c(1, 0.1))
-  ggsave(paste0(path, "/results/maps/prediction_height_", c, "_v46_final.png"),
+  ggsave(paste0(path, "/svd_dnn/svd_dnn_results/maps/prediction_height_", c, "_v46_revision.png"),
          hei_plot, dpi = 300, width = 12, height = 12, units = "in")
   
   legend <- cowplot::get_legend(lai_plot_list[[1]] + theme(legend.position = "bottom",
-                                                           legend.title = element_text(hjust = 1)))
+                                                           legend.title = element_text(hjust = 5)))
   p_grid <- cowplot::plot_grid(plotlist = lai_plot_list, ncol = 3)
   lai_plot <- cowplot::plot_grid(p_grid, legend, ncol = 1, rel_heights = c(1, 0.1))
-  ggsave(paste0(path, "/results/maps/prediction_lai_", c, "_v46_final.png"),
+  ggsave(paste0(path, "/svd_dnn/svd_dnn_results/maps/prediction_lai_", c, "_v46_revision.png"),
          lai_plot, dpi = 300, width = 12, height = 12, units = "in")
   
   
@@ -209,15 +258,34 @@ for(c in rcps){
                                                          legend.title = element_text(hjust = 1)))
     p_grid <- cowplot::plot_grid(plotlist = plot_list, ncol = 3)
     csi_plot <- cowplot::plot_grid(p_grid, legend, ncol = 1, rel_heights = c(1, 0.1))
-    ggsave(paste0(path, "/results/maps/prediction_csi_", c, "_all_v46_final.png"),
+    ggsave(paste0(path, "/svd_dnn/svd_dnn_results/maps/prediction_csi_", c, "_all_v46_revision.png"),
            csi_plot, dpi = 300, width = 12, height = 12, units = "in")
     
     
   }
   
+  
   # write out the raster stack with results
-  writeRaster(rast_all, paste0(path, "/results/rasters/csi_rast_new_", c, ".tif"), overwrite = T)
-
+  if(c == "rcp_8_5"){
+    writeRaster(rast_all, paste0(path, "/results/rasters/figure2.tif"), overwrite = T)
+    writeRaster(rast_1, paste0(path, "/results/rasters/figureS7.tif"), overwrite = T)
+    writeRaster(rast_2, paste0(path, "/results/rasters/figureS8.tif"), overwrite = T)
+  }else{
+    if(c == "rcp_4_5"){
+      writeRaster(rast_all, paste0(path, "/results/rasters/figureS5.tif"), overwrite = T)
+      writeRaster(rast_1, paste0(path, "/results/rasters/figureS9.tif"), overwrite = T)
+      writeRaster(rast_2, paste0(path, "/results/rasters/figureS10.tif"), overwrite = T)
+    }else{
+      writeRaster(rast_all, paste0(path, "/results/rasters/figureS6.tif"), overwrite = T)
+      writeRaster(rast_1, paste0(path, "/results/rasters/figureS11.tif"), overwrite = T)
+      writeRaster(rast_2, paste0(path, "/results/rasters/figureS12.tif"), overwrite = T)
+  
+    }
+  }
+ 
+  minmax <- do.call(rbind, min_max)
+  print(minmax)
 }# close RCPS
+
 
 ### end
